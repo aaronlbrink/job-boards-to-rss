5927c129f3481b3db4d79e379408aafb3d4ee2dd
Aaron Brink <6415108+aaronlbrink@users.noreply.github.com>
2024-11-13 09:33:40 -0500

    Add support for variable definitions in boards

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ from rss import convert_to_rss
 from syndication_types import ApiDefinition, BoardConfig
 
 
+def hydrate_api_definition(api_definition: ApiDefinition,
+                           api_vars: Dict) -> ApiDefinition:
+  hydrated_definition = json.loads(json.dumps(api_definition))
+
+  if "vars" not in hydrated_definition:
+    return hydrated_definition
+
+  vars_to_replace = hydrated_definition["vars"]
+  definition_str = json.dumps(hydrated_definition)
+
+  for var_name, var_placeholder in vars_to_replace.items():
+    if var_name in api_vars:
+      definition_str = definition_str.replace(
+          var_placeholder, api_vars[var_name])
+
+  return json.loads(definition_str)
+
+
 def get_board_name_from_uri(api_url: str) -> str:
   board_pattern = re.compile(r"boards/([^/]+)/")
   board_match = board_pattern.search(api_url)
@@ -49,9 +67,14 @@ def main():
     board_name = get_board_name_from_uri(api["board_uri"])
     print(f"----Board name: {board_name}-----")
     adapter = api_definitions[api["adapter"]]
+    api_def = None
+    if 'api_vars' in api:
+      api_def = hydrate_api_definition(adapter, api['api_vars'])
+    else:
+      api_def = adapter
     uri = api["board_uri"]
-    jobs = fetch_jobs_from_board(uri, adapter)
-    standarized_jobs = standardize_jobs(jobs, adapter, api["company_name"])
+    jobs = fetch_jobs_from_board(uri, api_def)
+    standarized_jobs = standardize_jobs(jobs, api_def, api["company_name"])
     (filtered_jobs, filter_stats) = filter_jobs(standarized_jobs, criteria)
     print(filter_stats)
     all_jobs.extend(filtered_jobs)
